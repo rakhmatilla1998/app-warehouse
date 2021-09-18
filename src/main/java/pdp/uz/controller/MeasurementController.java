a49d6b35c604ea35b54610194a7178f0bc1b73b8
Rakhmatilla <raxmatilla.tuganov1998@gmail.com>
2021-09-18 10:40:14 +0500

    sad

---
+++ src/main/java/pdp/uz/controller/MeasurementController.java
@@ -0,0 +1,9 @@
+package pdp.uz.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/measurement")
+public class MeasurementController {
+}
